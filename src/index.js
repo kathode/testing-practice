588e821e2a3b3ec3c8fe8a89cdd334d9e7065fc4
Kathode <work.qiyan@gmail.com>
2025-07-24 21:35:27 +1000

    feat: add analyzeArray function

--- src/index.js
+++ src/index.js
@@ -51,3 +51,12 @@ export const caesarCipher = (string, shifter) => {
 
   return output.join("");
 };
+
+export const anaylzeArray = (arr) => {
+  const length = arr.length;
+  const average = arr.reduce((arr, curr) => arr + curr) / length;
+  const min = arr.sort((a, b) => a - b)[0];
+  const max = arr.sort((a, b) => b - a)[0];
+
+  return { length, average, min, max };
+};
